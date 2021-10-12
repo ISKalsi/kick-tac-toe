a272546569cf3d53ee01d60f52f2a2bc84a1dd29
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2021-10-13 04:09:22 +0530

    fix text alignment ("yourTurn")

---
+++ playAgain.gd
@@ -0,0 +1,7 @@
+extends Button
+
+func _ready():
+	visible = true
+
+func _on_playAgain_pressed():
+	print("play again")
--- yourTurn.gd
+++ yourTurn.gd
@@ -1,29 +1,33 @@
-extends RichTextLabel
+extends Label
 
-const PLAYER1_X = 48
-const PLAYER2_X = 388
-const FINISHED_X = 200
+const PLAYER1_X = 10
+const PLAYER2_X = 325
+const FINISHED_X = 180
 
 var x_coord = PLAYER1_X
+var game_finished = false
 
 func _process(delta):
 	rect_global_position.x = lerp(rect_global_position.x, x_coord, 5 * delta)
 
 func _on_Dollset1_turn_over(did_win):
 	if did_win:
+		game_finished = true
 		x_coord = FINISHED_X
 		text = "Player 1 Won"
-	else:
+	elif !game_finished:
 		x_coord = PLAYER2_X
 
 func _on_Dollset2_turn_over(did_win):
 	if did_win:
+		game_finished = true
 		x_coord = FINISHED_X
 		text = "Player 2 Won"
-	else:
+	elif !game_finished:
 		x_coord = PLAYER1_X
 
 
 func _on_Dollset_game_tie():
+	game_finished = true
 	x_coord = FINISHED_X
 	text = "Game tie"
